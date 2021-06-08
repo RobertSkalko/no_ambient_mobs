4efd942b334d656058a65bb66266c95eb6d834ba
robert trebor <treborx555@gmail.com>
2021-06-08 19:14:04 +0200

    Initial commit

---
+++ src/main/java/com/robertx22/no_more_ambient_mobs/main/CommonInit.java
@@ -0,0 +1,11 @@
+package com.robertx22.no_more_ambient_mobs.main;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CommonInit implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.print("Ambient Mobs disabled. They will no longer spawn");
+    }
+}
